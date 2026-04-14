b14ae2e1b24c4000027f09695f12700f881c158b
ahmad <ahmadrazakhatha550@gmail.com>
2026-04-14 21:11:06 +0530

    Add task creation feature

--- task-tracker/task_tracker.py
+++ task-tracker/task_tracker.py
@@ -1,1 +1,12 @@
-﻿
+﻿# Simple Task Tracker
+
+tasks = []
+
+def add_task(task_name):
+    tasks.append({"name": task_name, "done": False})
+    print(f"Task added: {task_name}")
+
+# Test it
+add_task("Learn Python")
+add_task("Learn Git")
+print(tasks)
